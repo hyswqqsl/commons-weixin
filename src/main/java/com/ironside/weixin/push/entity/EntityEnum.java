0408b78d75c4c9e8effd7efba336ad42ba7f10c8
雪庭 <119238122@qq.com>
2015-04-07 22:20:47 +0800

    修改entityEnum

--- src/main/java/com/ironside/weixin/push/entity/EntityEnum.java
+++ src/main/java/com/ironside/weixin/push/entity/EntityEnum.java
@@ -7,42 +7,52 @@ package com.ironside.weixin.push.entity;
  * @since 1.0
  */
 public enum EntityEnum {
-
-	TEXT("text", null, "文本消息"),
-	IMAGE("image", null, "图像消息"),
-	VOICE("voice", null, "语音消息"),
-	VIDEO("video", null, "视频消息"), 
-	SHORTVIDEO("shortvideo", null, "小视频消息"),
-	LOCATION("location", null, "地理位置消息"),
-	LINK("link", null, "链接消息"),
-	EVENT_SUBSCRIBE("event", "subscribe", "关注/取消关注-订阅事件"),
-	VENT_UNSUBSCRIBE("event", "unsubscribe", "关注/取消关注-取消订阅事件"),
-	EVENT_SCAN_SUBSCRIBE("event", "subscribe", "扫描带参数二维码-用户未关注时，进行关注后的事件"),
-	EVENT_SCAN("event", "scan", "扫描带参数二维码-用户已关注时的事件"),
-	EVENT_LOCATION("event", "location", "上报地理位置事件"),
-	EVENT_CLICK("event", "click", "自定义菜单-点击菜单拉取消息时的事件"),
-	EVENT_VIEW("event", "view", "自定义菜单-点击菜单跳转链接时的事件");
+	/** 文本消息 */
+	TEXT("text", null),
+	/** 图像消息 */
+	IMAGE("image", null),
+	/** 语音消息 */
+	VOICE("voice", null),
+	/** 视频消息 */
+	VIDEO("video", null),
+	/** 小视频消息 */
+	SHORTVIDEO("shortvideo", null),
+	/** 地理位置消息 */
+	LOCATION("location", null),
+	/** 链接消息 */
+	LINK("link", null),
+	/** 关注/取消关注-订阅事件 */
+	EVENT_SUBSCRIBE("event", "subscribe"),
+	/** 关注/取消关注-取消订阅事件 */
+	VENT_UNSUBSCRIBE("event", "unsubscribe"),
+	/** 扫描带参数二维码-用户未关注时，进行关注后的事件 */
+	EVENT_SCAN_SUBSCRIBE("event", "subscribe"),
+	/** 扫描带参数二维码-用户已关注时的事件 */
+	EVENT_SCAN("event", "scan"),
+	/** 上报地理位置事件 */
+	EVENT_LOCATION("event", "location"),
+	/** 自定义菜单-点击菜单拉取消息时的事件 */
+	EVENT_CLICK("event", "click"),
+	/** 自定义菜单-点击菜单跳转链接时的事件 */
+	EVENT_VIEW("event", "view");
 	
 	/** 消息类型 */
 	private String msgType;
 	/** 事件类型 */
 	private String event;
-	/** 消息说明 */
-	private String remark;	
 
 	/**
 	 * 消息类型构造函数
 	 * @param msgType 消息类型
 	 * @param remark 消息说明
 	 */
-	private EntityEnum(String msgType, String event, String remark) {
+	private EntityEnum(String msgType, String event) {
 		this.msgType = msgType;
 		if (event==null) {
 			this.event = "";
 		} else {
 			this.event = event;
 		}
-		this.remark = remark;
 	}
 
 	/**
@@ -59,14 +69,6 @@ public enum EntityEnum {
 	 */
 	public String getEvent() {
 		return event;
-	}
-
-	/**
-	 * 取得消息说明
-	 * @return 消息说明
-	 */
-	public String getRemark() {
-		return remark;
 	}
 	
 }
