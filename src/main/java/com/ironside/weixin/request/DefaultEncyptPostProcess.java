a3fd483f6c2ab132cbf1a9a9400a5ffd617e3cb4
flysic <119238122@qq.com>
2015-05-07 16:53:34 +0800

    开始crypt

--- src/main/java/com/ironside/weixin/request/DefaultEncyptPostProcess.java
+++ src/main/java/com/ironside/weixin/request/DefaultEncyptPostProcess.java
@@ -1,6 +1,12 @@
 package com.ironside.weixin.request;
 
+import java.io.ByteArrayInputStream;
+
 import org.springframework.util.Assert;
+
+import com.qq.weixin.mp.aes.AesException;
+import com.qq.weixin.mp.aes.WXBizMsgCrypt;
+import com.waukeen.weixin.msg.MessageUtil;
 
 /**
  * POST方式推送给微信公众账号的加密消息处理，具体实现消息加密、解密。
@@ -8,6 +14,13 @@ import org.springframework.util.Assert;
  * @sine 1.0 at 2015年4月3日
  */
 public class DefaultEncyptPostProcess extends AbstractEncyptPostProcess {
+	
+	/** 公众平台上，开发者设置的token */
+	private String token;
+	/** 公众平台上，开发者设置的EncodingAESKey */
+	private String encodingAesKey;
+	/** 公众平台appid */
+	private String appId;	
 
 	/** 屏蔽默认构造函数 */
 	@SuppressWarnings("unused")
@@ -30,22 +43,28 @@ public class DefaultEncyptPostProcess extends AbstractEncyptPostProcess {
 		this.appId = appId;
 	}
 
-	/** 公众平台上，开发者设置的token */
-	private String token;
-	/** 公众平台上，开发者设置的EncodingAESKey */
-	private String encodingAesKey;
-	/** 公众平台appid */
-	private String appId;
-
 	@Override
-	protected String decypt(String signature, String timeStamp, String nonce,
-			String postData) {
-		return null;
+	protected String decypt(String signature, String timeStamp, String nonce,	String postData) {
+		String mingwen = null;
+		//解密，并解析XML
+		try{
+			pc = new WXBizMsgCrypt(token, encodingAesKey, appId);
+			mingwen = pc.decryptMsg(msgSignature, timeStamp, nonce, postData);
+		}catch (AesException e) {
+			if(e.getCode()==AesException.IllegalAesKey){
+				//试验老的AESKEY
+				pc = new WXBizMsgCrypt(token, encodingAesOldKey, appId);
+				mingwen = pc.decryptMsg(msgSignature, timeStamp, nonce, postData);
+			}else{
+				throw e;
+			}
+		}
+		//.encryptMsg(requestMap.get("Encrypt"), timestamp, nonce);
+		requestMap=MessageUtil.parseXml(new ByteArrayInputStream(mingwen.getBytes("UTF-8")));
 	}
 
 	@Override
-	protected String encryption(String responseData, String timeStamp,
-			String nonce) {
+	protected String encryption(String responseData, String timeStamp, String nonce) {
 		return null;
 	}
 
